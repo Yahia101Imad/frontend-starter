4d2ec79d75dc9ea2018bc0d7958258ec641f8cab
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-07-01 11:39:48 +0100

    feat: Add Toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
 
 import { router } from "./router";
 
 import "./styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+
+    <Toaster position="top-right" richColors closeButton />
+  </React.StrictMode>,
 );
